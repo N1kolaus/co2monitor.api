161c44648fd0ef6fb68343f9e722e2c32ea0d55d
Johannes <johannes.leyrer+git@googlemail.com>
2023-08-24 11:50:36 +0200

    add env and db to main

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/fminister/co2monitor.api/db"
+	"github.com/fminister/co2monitor.api/initializers"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	initializers.LoadEnvVariables()
+	db.ConnectToDb()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run()
 }
+
+// CompileDaemon -command="./co2monitor.api
