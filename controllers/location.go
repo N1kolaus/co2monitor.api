4d3273a92d780459513353ee21d2d0f64c65ba0b
Johannes <johannes.leyrer+git@googlemail.com>
2023-08-25 17:55:09 +0200

    changed for testing

--- controllers/location.go
+++ controllers/location.go
@@ -3,11 +3,25 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/fminister/co2monitor.api/db/db_calls"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-func GetLocation(c *gin.Context) {
+type APIEnv struct {
+	DB *gorm.DB
+}
+
+func (a *APIEnv) GetLocations(c *gin.Context) {
+	locations, err := db_calls.GetLocation(a.DB)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"location": "location",
+		"location": locations,
 	})
 }
--- routes/location_routes.go
+++ routes/location_routes.go
@@ -2,16 +2,21 @@ package routes
 
 import (
 	"github.com/fminister/co2monitor.api/controllers"
+	"github.com/fminister/co2monitor.api/db"
 	"github.com/fminister/co2monitor.api/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func locationRoutes(superRoute *gin.RouterGroup) {
+	controllers := &controllers.APIEnv{
+		DB: db.GetDB(),
+	}
+
 	locationRouter := superRoute.Group("/location")
 	{
 		locationRouter.Use(middleware.RequireAuth("X_API_KEY"))
 		{
-			locationRouter.GET("/", controllers.GetLocation)
+			locationRouter.GET("/", controllers.GetLocations)
 		}
 	}
 }
