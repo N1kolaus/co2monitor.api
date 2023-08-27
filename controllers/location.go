40355d38384cf0ff6ca0e4211a4021250f78fb37
Johannes <johannes.leyrer+git@googlemail.com>
2023-08-27 13:44:07 +0200

    added update location

--- controllers/location.go
+++ controllers/location.go
@@ -9,12 +9,11 @@ import (
 	ex "github.com/fminister/co2monitor.api/extensions"
 	"github.com/fminister/co2monitor.api/models"
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
 
-type APIEnv struct {
-	DB *gorm.DB
-}
+// type APIEnv struct {
+// 	DB *gorm.DB
+// }
 
 func (a *APIEnv) GetLocations(c *gin.Context) {
 	locations, err := db_calls.GetLocation(a.DB)
@@ -78,5 +77,47 @@ func (a *APIEnv) CreateLocation(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{
 		"locations": locations,
+	})
+}
+
+func (a *APIEnv) UpdateLocation(c *gin.Context) {
+	locationId := c.Param("id")
+
+	if _, err := db_calls.GetLocationById(a.DB, locationId); err != nil {
+		log.Errorf(`Could not find location by id. id: "%s"; Error: "%s"`, locationId, err)
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Could not find location by id.",
+		})
+		return
+	}
+
+	var location models.Location
+	if err := c.ShouldBindJSON(&location); err != nil {
+		log.Errorf(`Could not parse location details from body. Error: "%s"`, err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Could not parse location details from body.",
+		})
+		return
+	}
+
+	if err := ex.Validator(models.Location{}).Validate(location); err != nil {
+		log.Errorf(`Could parse location from body. Error: "%s"`, err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	location, err := db_calls.UpdateLocation(a.DB, location)
+	if err != nil {
+		log.Errorf(`Could not update location in db. Location: "%#v" Error: "%s"`, location, err)
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Could not update location.",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"location": location,
 	})
 }
