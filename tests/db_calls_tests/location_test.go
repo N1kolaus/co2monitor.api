d80c39d2b1b9226d2583063778bc0e05e8b62acd
Johannes <johannes.leyrer+git@googlemail.com>
2023-08-27 16:23:34 +0200

    refined db tests

--- tests/db_calls_tests/location_test.go
+++ tests/db_calls_tests/location_test.go
@@ -25,108 +25,104 @@ func TestGetLocation_ShouldReturnAllLocations(t *testing.T) {
 	assert.Equal(t, tests.Locations[1].Name, result[1].Name)
 }
 
-func TestGetLocationBySearch(t *testing.T) {
-	t.Run("search by id and find one", func(t *testing.T) {
-		f := tests.BaseFixture{}
-		f.Setup(t)
-		f.AddDummyData(t)
-		defer f.Teardown(t)
+func TestGetLocationBySearch_ShouldFindOneById(t *testing.T) {
+	f := tests.BaseFixture{}
+	f.Setup(t)
+	f.AddDummyData(t)
+	defer f.Teardown(t)
 
-		result, err := db_calls.GetLocationBySearch(f.Db, "2", "")
+	result, err := db_calls.GetLocationBySearch(f.Db, "2", "")
 
-		require.NoError(t, err)
-		assert.Equal(t, tests.Locations[1].Name, result[0].Name)
-		assert.Equal(t, 1, len(result))
-	})
-
-	t.Run("search by name and find one", func(t *testing.T) {
-		f := tests.BaseFixture{}
-		f.Setup(t)
-		f.AddDummyData(t)
-		defer f.Teardown(t)
-
-		result, err := db_calls.GetLocationBySearch(f.Db, "", tests.Locations[1].Name)
-
-		require.NoError(t, err)
-		assert.Equal(t, tests.Locations[1].Name, result[0].Name)
-		assert.Equal(t, 1, len(result))
-	})
-
-	t.Run("search by id and name and find both", func(t *testing.T) {
-		f := tests.BaseFixture{}
-		f.Setup(t)
-		f.AddDummyData(t)
-		defer f.Teardown(t)
-
-		result, err := db_calls.GetLocationBySearch(f.Db, "1", tests.Locations[1].Name)
-
-		require.NoError(t, err)
-		assert.Equal(t, tests.Locations[0].Name, result[0].Name)
-		assert.Equal(t, tests.Locations[1].Name, result[1].Name)
-		assert.Equal(t, 2, len(result))
-	})
-
-	t.Run("search by id and name and find none", func(t *testing.T) {
-		f := tests.BaseFixture{}
-		f.Setup(t)
-		f.AddDummyData(t)
-		defer f.Teardown(t)
-
-		result, err := db_calls.GetLocationBySearch(f.Db, "3", "not in db")
-
-		require.NoError(t, err)
-		assert.Equal(t, 0, len(result))
-	})
+	require.NoError(t, err)
+	assert.Equal(t, tests.Locations[1].Name, result[0].Name)
+	assert.Equal(t, 1, len(result))
 }
 
-func TestCreateLocation(t *testing.T) {
-	t.Run("create single location", func(t *testing.T) {
-		f := tests.BaseFixture{}
-		f.Setup(t)
-		defer f.Teardown(t)
+func TestGetLocationBySearch_ShouldFindOneByName(t *testing.T) {
+	f := tests.BaseFixture{}
+	f.Setup(t)
+	f.AddDummyData(t)
+	defer f.Teardown(t)
 
-		locationToInsert := []models.Location{tests.Locations[1]}
-		beforeInsert := []models.Location{}
-		f.Db.Find(&beforeInsert)
-		result, err := db_calls.CreateLocation(f.Db, locationToInsert)
+	result, err := db_calls.GetLocationBySearch(f.Db, "", tests.Locations[1].Name)
 
-		require.NoError(t, err)
-		assert.Equal(t, 0, len(beforeInsert))
-		assert.Equal(t, len(locationToInsert), len(result))
-		assert.Equal(t, tests.Locations[1].Name, result[0].Name)
-	})
+	require.NoError(t, err)
+	assert.Equal(t, tests.Locations[1].Name, result[0].Name)
+	assert.Equal(t, 1, len(result))
+}
 
-	t.Run("create multiple locations", func(t *testing.T) {
-		f := tests.BaseFixture{}
-		f.Setup(t)
-		defer f.Teardown(t)
+func TestGetLocationBySearch_ShouldReturnBothByIdAndName(t *testing.T) {
+	f := tests.BaseFixture{}
+	f.Setup(t)
+	f.AddDummyData(t)
+	defer f.Teardown(t)
 
-		locationToInsert := []models.Location{tests.Locations[0], tests.Locations[1]}
-		beforeInsert := []models.Location{}
-		f.Db.Find(&beforeInsert)
-		result, err := db_calls.CreateLocation(f.Db, locationToInsert)
+	result, err := db_calls.GetLocationBySearch(f.Db, "1", tests.Locations[1].Name)
 
-		require.NoError(t, err)
-		assert.Equal(t, 0, len(beforeInsert))
-		assert.Equal(t, len(locationToInsert), len(result))
-		assert.Equal(t, tests.Locations[0].Name, result[0].Name)
-		assert.Equal(t, tests.Locations[1].Name, result[1].Name)
-	})
+	require.NoError(t, err)
+	assert.Equal(t, tests.Locations[0].Name, result[0].Name)
+	assert.Equal(t, tests.Locations[1].Name, result[1].Name)
+	assert.Equal(t, 2, len(result))
+}
 
-	t.Run("empty location does not create new location", func(t *testing.T) {
-		f := tests.BaseFixture{}
-		f.Setup(t)
-		defer f.Teardown(t)
+func TestGetLocationBySearch_ShouldReturnNoneByIdAndName(t *testing.T) {
+	f := tests.BaseFixture{}
+	f.Setup(t)
+	f.AddDummyData(t)
+	defer f.Teardown(t)
 
-		locationToInsert := []models.Location{}
-		beforeInsert := []models.Location{}
-		f.Db.Find(&beforeInsert)
-		result, err := db_calls.CreateLocation(f.Db, locationToInsert)
+	result, err := db_calls.GetLocationBySearch(f.Db, "3", "not in db")
 
-		require.NoError(t, err)
-		assert.Equal(t, 0, len(beforeInsert))
-		assert.Equal(t, len(locationToInsert), len(result))
-	})
+	require.NoError(t, err)
+	assert.Equal(t, 0, len(result))
+}
+
+func TestCreateLocation_ShouldCreateSingleLocation(t *testing.T) {
+	f := tests.BaseFixture{}
+	f.Setup(t)
+	defer f.Teardown(t)
+
+	locationToInsert := []models.Location{tests.Locations[1]}
+	beforeInsert := []models.Location{}
+	f.Db.Find(&beforeInsert)
+	result, err := db_calls.CreateLocation(f.Db, locationToInsert)
+
+	require.NoError(t, err)
+	assert.Equal(t, 0, len(beforeInsert))
+	assert.Equal(t, len(locationToInsert), len(result))
+	assert.Equal(t, tests.Locations[1].Name, result[0].Name)
+}
+
+func TestCreateLocation_ShouldCreateMultipleLocations(t *testing.T) {
+	f := tests.BaseFixture{}
+	f.Setup(t)
+	defer f.Teardown(t)
+
+	locationToInsert := []models.Location{tests.Locations[0], tests.Locations[1]}
+	beforeInsert := []models.Location{}
+	f.Db.Find(&beforeInsert)
+	result, err := db_calls.CreateLocation(f.Db, locationToInsert)
+
+	require.NoError(t, err)
+	assert.Equal(t, 0, len(beforeInsert))
+	assert.Equal(t, len(locationToInsert), len(result))
+	assert.Equal(t, tests.Locations[0].Name, result[0].Name)
+	assert.Equal(t, tests.Locations[1].Name, result[1].Name)
+}
+
+func TestCreateLocation_ShouldNotCreateLocation(t *testing.T) {
+	f := tests.BaseFixture{}
+	f.Setup(t)
+	defer f.Teardown(t)
+
+	locationToInsert := []models.Location{}
+	beforeInsert := []models.Location{}
+	f.Db.Find(&beforeInsert)
+	result, err := db_calls.CreateLocation(f.Db, locationToInsert)
+
+	require.NoError(t, err)
+	assert.Equal(t, 0, len(beforeInsert))
+	assert.Equal(t, len(locationToInsert), len(result))
 }
 
 func TestUpdateLocation_ShouldUpdateLocation(t *testing.T) {
