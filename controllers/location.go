5a391edc3ea8978896c17f3bbf3d5d2bbedc86de
Johannes <johannes.leyrer+git@googlemail.com>
2023-08-27 16:40:25 +0200

    changed return values to simple models

--- controllers/location.go
+++ controllers/location.go
@@ -15,15 +15,11 @@ func (a *APIEnv) GetLocations(c *gin.Context) {
 	locations, err := db_calls.GetLocation(a.DB)
 	if err != nil {
 		log.Errorf(`Could not find any locations. Error: "%s"`, err)
-		c.JSON(http.StatusNotFound, gin.H{
-			"error": "Could not find any locations.",
-		})
+		c.JSON(http.StatusNotFound, "Could not find any locations.")
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"location": locations,
-	})
+	c.JSON(http.StatusOK, locations)
 }
 
 func (a *APIEnv) GetLocationBySearch(c *gin.Context) {
@@ -33,47 +29,35 @@ func (a *APIEnv) GetLocationBySearch(c *gin.Context) {
 	locations, err := db_calls.GetLocationBySearch(a.DB, id, name)
 	if err != nil {
 		log.Errorf(`Could not find any locations by id or name. id: "%s"; name: "%s"; Error: "%s"`, id, name, err)
-		c.JSON(http.StatusNotFound, gin.H{
-			"error": "Could not find any locations.",
-		})
+		c.JSON(http.StatusNotFound, "Could not find any locations.")
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"location": locations,
-	})
+	c.JSON(http.StatusOK, locations)
 }
 
 func (a *APIEnv) CreateLocation(c *gin.Context) {
 	var locations []models.Location
 	if err := c.ShouldBindJSON(&locations); err != nil {
 		log.Errorf(`Could not parse location from body. Error: "%s"`, err)
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Could not parse location from body.",
-		})
+		c.JSON(http.StatusBadRequest, "Could not parse location from body.")
 		return
 	}
 
 	if err := ex.Validator([]models.Location{}).Validate(locations); err != nil {
 		log.Errorf(`Could parse location from body. Error: "%s"`, err)
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err,
-		})
+		c.JSON(http.StatusBadRequest, err)
 		return
 	}
 
 	locations, err := db_calls.CreateLocation(a.DB, locations)
 	if err != nil {
 		log.Errorf(`Could not create location in db. Locations: "%#v" Error: "%s"`, locations, err)
-		c.JSON(http.StatusNotFound, gin.H{
-			"error": "Could not create location.",
-		})
+		c.JSON(http.StatusNotFound, "Could not create location.")
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"location": locations,
-	})
+	c.JSON(http.StatusCreated, locations)
 }
 
 func (a *APIEnv) UpdateLocation(c *gin.Context) {
@@ -81,41 +65,31 @@ func (a *APIEnv) UpdateLocation(c *gin.Context) {
 
 	if _, err := db_calls.GetLocationById(a.DB, locationId); err != nil {
 		log.Errorf(`Could not find location by id. id: "%s"; Error: "%s"`, locationId, err)
-		c.JSON(http.StatusNotFound, gin.H{
-			"error": "Could not find location by id.",
-		})
+		c.JSON(http.StatusNotFound, "Could not find location by id.")
 		return
 	}
 
 	var location models.Location
 	if err := c.ShouldBindJSON(&location); err != nil {
 		log.Errorf(`Could not parse location details from body. Error: "%s"`, err)
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Could not parse location details from body.",
-		})
+		c.JSON(http.StatusBadRequest, "Could not parse location details from body.")
 		return
 	}
 
 	if err := ex.Validator(models.Location{}).Validate(location); err != nil {
 		log.Errorf(`Could parse location from body. Error: "%s"`, err)
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err,
-		})
+		c.JSON(http.StatusBadRequest, err)
 		return
 	}
 
 	location, err := db_calls.UpdateLocation(a.DB, location)
 	if err != nil {
 		log.Errorf(`Could not update location in db. Location: "%#v" Error: "%s"`, location, err)
-		c.JSON(http.StatusNotFound, gin.H{
-			"error": "Could not update location.",
-		})
+		c.JSON(http.StatusNotFound, "Could not update location.")
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"location": location,
-	})
+	c.JSON(http.StatusOK, location)
 }
 
 func (a *APIEnv) DeleteLocation(c *gin.Context) {
@@ -124,20 +98,16 @@ func (a *APIEnv) DeleteLocation(c *gin.Context) {
 	location, err := db_calls.GetLocationById(a.DB, locationId)
 	if err != nil {
 		log.Errorf(`Could not find location by id. id: "%s"; Error: "%s"`, locationId, err)
-		c.JSON(http.StatusNotFound, gin.H{
-			"error": "Could not find location by id.",
-		})
+		c.JSON(http.StatusNotFound, "Could not find location by id.")
 		return
 	}
 
 	err = db_calls.DeleteLocation(a.DB, location)
 	if err != nil {
 		log.Errorf(`Could not delete location in db. Location: "%#v" Error: "%s"`, location, err)
-		c.JSON(http.StatusNotFound, gin.H{
-			"error": "Could not delete location.",
-		})
+		c.JSON(http.StatusNotFound, "Could not delete location.")
 		return
 	}
 
-	c.JSON(http.StatusNoContent, gin.H{})
+	c.JSON(http.StatusNoContent, nil)
 }
