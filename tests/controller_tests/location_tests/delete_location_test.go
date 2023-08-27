3ac323de36196e17e60e5f970c3fc7e63c989e14
Johannes <johannes.leyrer+git@googlemail.com>
2023-08-27 22:11:02 +0200

    added tests for delete controller

--- tests/controller_tests/location_tests/delete_location_test.go
+++ tests/controller_tests/location_tests/delete_location_test.go
@@ -1,1 +1,64 @@
 package tests
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/fminister/co2monitor.api/controllers"
+	"github.com/fminister/co2monitor.api/models"
+	"github.com/fminister/co2monitor.api/tests"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDeleteLocation_ShouldDeleteLocation(t *testing.T) {
+	f := tests.BaseFixture{}
+	f.Setup(t)
+	f.AddDummyData(t)
+	api := &controllers.APIEnv{DB: f.Db}
+	req, writer := tests.SetupLocationRouter(f.Db, http.MethodDelete, "/:id", "/1", api.DeleteLocation, nil)
+	defer f.Teardown(t)
+
+	body, err := io.ReadAll(writer.Body)
+	if err != nil {
+		assert.Error(t, err)
+	}
+	responseData := ""
+	if err := json.Unmarshal(body, &responseData); err != nil {
+		assert.Error(t, err)
+	}
+	expectedInDb := []models.Location{}
+	f.Db.Find(&expectedInDb)
+
+	assert.Equal(t, http.MethodDelete, req.Method, "HTTP request method error")
+	assert.Equal(t, http.StatusNoContent, writer.Code, "HTTP request status code error")
+	assert.Equal(t, len(expectedInDb), 1)
+	assert.Equal(t, expectedInDb[0].Name, tests.Locations[1].Name)
+}
+
+func TestDeleteLocation_ShouldReturnErrorUnknownId(t *testing.T) {
+	f := tests.BaseFixture{}
+	f.Setup(t)
+	f.AddDummyData(t)
+	api := &controllers.APIEnv{DB: f.Db}
+	req, writer := tests.SetupLocationRouter(f.Db, http.MethodDelete, "/:id", "/99", api.DeleteLocation, nil)
+	defer f.Teardown(t)
+
+	body, err := io.ReadAll(writer.Body)
+	if err != nil {
+		assert.Error(t, err)
+	}
+	errorMessage := ""
+	if err := json.Unmarshal(body, &errorMessage); err != nil {
+		assert.Error(t, err)
+	}
+	expectedErrorMessage := "Could not find location by id."
+	expectedInDb := []models.Location{}
+	f.Db.Find(&expectedInDb)
+
+	assert.Equal(t, http.MethodDelete, req.Method, "HTTP request method error")
+	assert.Equal(t, http.StatusNotFound, writer.Code, "HTTP request status code error")
+	assert.Equal(t, expectedErrorMessage, errorMessage)
+	assert.Equal(t, len(expectedInDb), len(tests.Locations))
+}
