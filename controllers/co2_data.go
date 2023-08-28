440be9ba3d0d3c9c00cd735888d0cbb84234d691
Johannes <johannes.leyrer+git@googlemail.com>
2023-08-28 11:59:10 +0200

    added first co2 controller logic and tests

--- controllers/co2_data.go
+++ controllers/co2_data.go
@@ -3,6 +3,10 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/charmbracelet/log"
+	"github.com/fminister/co2monitor.api/db/db_calls"
+	ex "github.com/fminister/co2monitor.api/extensions"
+	"github.com/fminister/co2monitor.api/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -22,7 +26,25 @@ func (a *APIEnv) GetLatestCo2Data(c *gin.Context) {
 }
 
 func (a *APIEnv) CreateCo2Data(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"data": "co2 data",
-	})
+	var co2Data []models.Co2Data
+	if err := c.ShouldBindJSON(&co2Data); err != nil {
+		log.Errorf(`Could not parse co2 data from body. Error: "%s"`, err)
+		c.JSON(http.StatusBadRequest, "Could not parse co2 data from body.")
+		return
+	}
+
+	if err := ex.Validator([]models.Co2Data{}).Validate(co2Data); err != nil {
+		log.Errorf(`Missing values in JSON. Error: "%s"`, err)
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	co2Data, err := db_calls.CreateCo2Data(a.DB, co2Data)
+	if err != nil {
+		log.Errorf(`Could not create co2 data in db. Co2Data: "%#v" Error: "%s"`, co2Data, err)
+		c.JSON(http.StatusBadRequest, "Could not create co2 data.")
+		return
+	}
+
+	c.JSON(http.StatusCreated, co2Data)
 }
--- models/co2_data.go
+++ models/co2_data.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Co2Data struct {
 	gorm.Model
-	CO2        int     `json:"co2"`
-	Temp       float32 `json:"temp"`
-	LocationID int     `json:"location_id"`
+	CO2        int     `g:"required" gorm:"not null;" json:"co2"`
+	Temp       float32 `g:"required" gorm:"not null;" json:"temp"`
+	LocationID int     `g:"required" gorm:"not null;" json:"location_id"`
 	Location   Location
 }
