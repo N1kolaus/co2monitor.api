e1d8e2e752d17c145714e9b1466a3ea50568a504
FMinister <johannes.leyrer+git@googlemail.com>
2024-03-27 12:46:29 +0100

    update co2 validator and model

--- internal/data/co2.go
+++ internal/data/co2.go
@@ -15,20 +15,20 @@ type Co2Data struct {
 	ID         int       `json:"id"`
 	CreatedAt  time.Time `json:"created_at"`
 	LocationID int64     `json:"location_id"`
-	Co2        int       `json:"co2,omitempty"`
-	Temp       float64   `json:"temp,omitempty"`
-	Humidity   int       `json:"humidity,omitempty"`
+	Co2        int       `json:"co2"`
+	Temp       float64   `json:"temp"`
+	Humidity   int       `json:"humidity"`
 }
 
 func ValidateCo2Data(v *validator.Validator, co2Data *Co2Data) {
 	v.Check(co2Data.LocationID > 0, "location_id", "must greater than zero")
 
-	v.Check(co2Data.Co2 >= 0, "co2", "must be provided or set to zero")
+	v.Check(co2Data.Co2 >= 0, "co2", "must be greater than or equal to zero ppm")
 	v.Check(co2Data.Co2 < 5000, "co2", "must be less than 5000 ppm")
 
 	v.Check(co2Data.Temp >= -100, "temp", "must be greater than or equal to -100 degrees Celsius")
 	v.Check(co2Data.Temp <= 100, "temp", "must be less than or equal to 100 degrees Celsius")
 
 	v.Check(co2Data.Humidity >= 0, "humidity", "must be greater than or equal to zero percent")
-	v.Check(co2Data.Humidity <= 0, "humidity", "must be less than or equal to 100 percent")
+	v.Check(co2Data.Humidity <= 100, "humidity", "must be less than or equal to 100 percent")
 }
