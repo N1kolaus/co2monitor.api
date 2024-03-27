f532fb593bfdb8b6a48b1221fb52bfe296c045cb
FMinister <johannes.leyrer+git@googlemail.com>
2024-03-27 14:30:53 +0100

    add locations model and validator

--- internal/data/location.go
+++ internal/data/location.go
@@ -2,8 +2,24 @@ package data
 
 import (
 	"database/sql"
+	"time"
+
+	"github.com/FMinister/co2monitor-api/internal/validator"
 )
 
 type LocationModel struct {
 	DB *sql.DB
+}
+
+type Location struct {
+	ID        int       `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdateAt  time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+}
+
+func ValidateLocationData(v *validator.Validator, location *Location) {
+	v.Check(location.Name != "", "name", "must be provided")
+	v.Check(len(location.Name) > 3, "name", "must be longer than 3 bytes")
+	v.Check(len(location.Name) <= 500, "name", "must not be more than 500 bytes long")
 }
